e06f3da2215ec40ea807d5d28445d7b12dd11376
Manolis Dermitzakis <mgdermitz@gmail.com>
2023-10-07 20:45:44 +0300

    Move Javascript package to it's own folder

--- clean.sh
+++ clean.sh
@@ -1,1 +1,7 @@
-rm ./antlr4.jar src/Generated/ArcscriptLexer.js src/Generated/ArcscriptParser.js src/Generated/ArcscriptParserVisitor.js
+rm ./antlr4.jar
+
+rm JavaScript/src/Generated/ArcscriptLexer.js JavaScript/src/Generated/ArcscriptParser.js JavaScript/src/Generated/ArcscriptParserVisitor.js
+
+rm -rf Cpp/src/Generated
+
+rm -rf CSharp/src/Generated
--- generate.sh
+++ generate.sh
@@ -1,9 +1,20 @@
 if [ ! -f ./antlr4.jar ]
 then  
-  wget wget -O antlr4.jar https://www.antlr.org/download/antlr-4.13.1-complete.jar
+  curl https://www.antlr.org/download/antlr-4.13.1-complete.jar -o antlr4.jar
 fi
 cd grammar
-java -Xmx500M -cp ../antlr4.jar org.antlr.v4.Tool -Dlanguage=JavaScript ArcscriptLexer.g4 ArcscriptParser.g4 -visitor -no-listener -o ./temp
-mkdir -p ../src/Generated
-cp temp/*.js ../src/Generated/.
-rm -rf ./temp
+java -Xmx500M -cp ../antlr4.jar org.antlr.v4.Tool -Dlanguage=JavaScript ArcscriptLexer.g4 ArcscriptParser.g4 -visitor -no-listener -o ./JavaScript
+mkdir -p ../JavaScript/src/Generated
+cp JavaScript/*.js ../JavaScript/src/Generated/.
+rm -rf ./JavaScript
+
+java -Xmx500M -cp ../antlr4.jar org.antlr.v4.Tool -Dlanguage=Cpp ArcscriptLexer.g4 ArcscriptParser.g4 -visitor -no-listener -o ./Cpp
+mkdir -p ../Cpp/src/Generated
+cp Cpp/*.cpp ../Cpp/src/Generated
+cp Cpp/*.h ../Cpp/src/Generated
+rm -rf ./Cpp
+
+java -Xmx500M -cp ../antlr4.jar org.antlr.v4.Tool -Dlanguage=CSharp ArcscriptLexer.g4 ArcscriptParser.g4 -visitor -no-listener -o ./CSharp
+mkdir -p ../CSharp/src/Generated
+cp CSharp/*.cs ../CSharp/src/Generated/.
+rm -rf ./CSharp
