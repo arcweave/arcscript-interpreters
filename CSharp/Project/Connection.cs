58e628bab9c0773fa7333c3f1b16c7dfbad2aaae
Emmanouil Dermitzakis <manolis@arcweave.com>
2024-02-19 21:04:38 +0200

    feat: initial CSharp interpreter

---
+++ CSharp/Project/Connection.cs
@@ -0,0 +1,27 @@
+﻿using Arcweave.Interpreter.INodes;
+
+namespace Arcweave.Project;
+
+public partial class Connection
+{
+    public string Id { get; }
+    public string RawLabel { get; }
+    public string RuntimeLabel { get; }
+    public INode Source { get; }
+    public INode Target { get; }
+    public Project Project { get; }
+    public void Set(string label, INode source, INode target)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RunLabelScript()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Path ResolvePath(Path p)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ CSharp/Project/HelperClasses.cs
@@ -0,0 +1,23 @@
+﻿namespace Arcweave.Project;
+
+public partial class Options
+{
+    public Element Element { get; set; }
+    public List<Path> Paths { get; set; }
+}
+
+public partial class Path
+{
+    public string label { get; set; }
+    public Element TargetElement { get; set; }
+    public List<Connection> _connections { get; set; }
+    public void AppendConnection(Connection connection)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ExecuteAppendedConnectionLabels()
+    {
+        throw new NotImplementedException();
+    }
+}
